d9de7533f88853a16378e9594a7c39684b1cbc3f
chiraag <chiraag@thesolarlabs.com>
2021-06-09 01:50:23 +0530

    sync fetch videos function

---
+++ fampay/cron.py
@@ -0,0 +1,22 @@
+from videos.models import Video
+from django.conf import settings
+import requests
+import aiohttp
+import asyncio
+
+url = f'https://www.googleapis.com/youtube/v3/search?key={settings.KEY}&q={settings.SEARCH_QUERY}&order={settings.ORDER_BY}&type={settings.TYPE}&publishedAfter={settings.PUBLISHING_START_TIME}&part={settings.PART}&maxResults={settings.MAX_RESULTS}'
+
+async def fetch_videos():
+    res = requests.get(url)
+    results = res.json()['items']
+    for item in results:
+        title = item['snippet']['title']
+        description = item['snippet']['description']
+        publishing_datetime = item['snippet']['publishedAt']
+        thumbnails = item['snippet']['thumbnails']
+        video_id = item['id']['videoId']
+        is_video_present = Video.objects.filter(video_id = video_id).first()
+        if not is_video_present:
+            Video.objects.create(title = title, description = description, publishing_datetime = publishing_datetime,thumbnails = thumbnails, video_id = video_id)
+        else:
+            break
---
+++ videos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from djangodeletes.softdeletes import SoftDeletable, SoftDeleteQuerySet, SoftDeleteManager
+from channel import models as channel_models
+from django.contrib.postgres.fields import JSONField
+
+class Video(SoftDeletable, models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    publishing_datetime = models.DateTimeField(null=True, blank=True)
+    thumbnails = JSONField(null=True)
+    video_id = models.CharField(max_length=50, null=True, blank=True)
+
+    objects = SoftDeleteManager.from_queryset(SoftDeleteQuerySet)()
+
+    def __str__(self):
+        return str(self.title)
