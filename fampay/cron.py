b0d882402a638956379f650e59c1dfc9153ea052
chiraag <chiraag@thesolarlabs.com>
2021-06-09 11:17:22 +0530

    an API to search the videos using their title and description

--- fampay/cron.py
+++ fampay/cron.py
@@ -6,7 +6,8 @@ import asyncio
 
 url = f'https://www.googleapis.com/youtube/v3/search?key={settings.KEY}&q={settings.SEARCH_QUERY}&order={settings.ORDER_BY}&type={settings.TYPE}&publishedAfter={settings.PUBLISHING_START_TIME}&part={settings.PART}&maxResults={settings.MAX_RESULTS}'
 
-async def fetch_videos():
+def fetch_videos():
+    Video.objects.create(title = 'aa')
     res = requests.get(url)
     results = res.json()['items']
     for item in results:
@@ -18,5 +19,3 @@ async def fetch_videos():
         is_video_present = Video.objects.filter(video_id = video_id).first()
         if not is_video_present:
             Video.objects.create(title = title, description = description, publishing_datetime = publishing_datetime,thumbnails = thumbnails, video_id = video_id)
-        else:
-            break
--- videos/viewsets.py
+++ videos/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from videos.serializers import VideoSerializer
 from .models import Video
+from django.db.models import Q
 
 
 
@@ -8,6 +9,14 @@ class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
 
+    def get_queryset(self):
+        query = self.request.query_params.get("query", None)
+        if query:
+            queryset = Video.objects.filter(Q(title__icontains = query) | Q(description__icontains=query)).order_by('-publishing_datetime')
+        else:
+            queryset = Video.objects.all().order_by('-publishing_datetime')
+        return queryset
+
     def retrieve(self, request, pk=None):
-        queryset = Video.objects.all().order_by('publishing_datetime')
+        queryset = Video.objects.all().order_by('-publishing_datetime')
         return queryset
